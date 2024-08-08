acd13fa64bec791352522cd96a217ddd14ca59ea
per_terra <53376589+Per-Terra@users.noreply.github.com>
2024-08-08 19:58:14 +0900

    Fix packages won't be loaded

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="shu_syllabus",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     install_requires=[
         "beautifulsoup4",
         "requests",
